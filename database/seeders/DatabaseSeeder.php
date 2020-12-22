a0b9657f526113967497b4187ee0cdc56d9ead39
Koon <vlgbao97@gmail.com>
2020-12-23 01:19:34 +0700

    update composer script

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,20 +14,20 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // User::factory(2)->create();
-        // $this->call(RolesTableSeeder::class);
-        // $this->call(UsersTableSeeder::class);
-        // $this->call(RegionsTableSeeder::class);
-        // $this->call(CinemasTableSeeder::class);
-        // $this->call(ScreensTableSeeder::class);
-        // $this->call(SeatRowsTableSeeder::class);
-        // $this->call(SeatsTableSeeder::class);
-        // $this->call(CastersTableSeeder::class);
-        // $this->call(CategoriesTableSeeder::class);
-        // $this->call(LanguegesTableSeeder::class);
-        // $this->call(MoviesTableSeeder::class);
-        // $this->call(MovieDetailsTableSeeder::class);
-        // $this->call(MovieScreensTableSeeder::class);
-        // $this->call(ProductsTableSeeder::class);
+        $this->call(RolesTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
+        $this->call(RegionsTableSeeder::class);
+        $this->call(CinemasTableSeeder::class);
+        $this->call(ScreensTableSeeder::class);
+        $this->call(SeatRowsTableSeeder::class);
+        $this->call(SeatsTableSeeder::class);
+        $this->call(CastersTableSeeder::class);
+        $this->call(CategoriesTableSeeder::class);
+        $this->call(LanguegesTableSeeder::class);
+        $this->call(MoviesTableSeeder::class);
+        $this->call(MovieDetailsTableSeeder::class);
+        $this->call(MovieScreensTableSeeder::class);
+        $this->call(ProductsTableSeeder::class);
 
 
     }
